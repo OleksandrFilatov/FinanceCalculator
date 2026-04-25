9d1ad58605632670b505d64b1de0a3be611c3a53
OleksandrFilatov <alexfil19998@gmail.com>
2026-04-26 01:25:38 +0200

    add monthly report entity

---
+++ src/com/alex/financetracker/entity/MonthlyReport.java
@@ -0,0 +1,41 @@
+package com.alex.financetracker.entity;
+
+
+public class MonthlyReport {
+
+    private int year;
+    private int month;
+    private double totalIncome;
+    private double totalCardExpense;
+    private double totalCashExpense;
+    private double totalExpense;
+    private double balance;
+    private double cumulativeBalance;
+
+    public MonthlyReport(int year, int month, double totalIncome, double totalCardExpense,
+                         double totalCashExpense, double totalExpense, double balance,
+                         double cumulativeBalance) {
+        this.year = year;
+        this.month = month;
+        this.totalIncome = totalIncome;
+        this.totalCardExpense = totalCardExpense;
+        this.totalCashExpense = totalCashExpense;
+        this.totalExpense = totalExpense;
+        this.balance = balance;
+        this.cumulativeBalance = cumulativeBalance;
+    }
+
+    @Override
+    public String toString() {
+        return "MonthlyReport{" +
+                "year=" + year +
+                ", month=" + month +
+                ", totalIncome=" + totalIncome +
+                ", totalCardExpense=" + totalCardExpense +
+                ", totalCashExpense=" + totalCashExpense +
+                ", totalExpense=" + totalExpense +
+                ", balance=" + balance +
+                ", cumulativeBalance=" + cumulativeBalance +
+                '}';
+    }
+}
