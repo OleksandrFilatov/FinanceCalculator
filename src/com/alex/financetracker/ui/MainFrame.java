b7ac6bf3c777a37c10a41bc7d9742bbe11ab8e66
OleksandrFilatov <alexfil19998@gmail.com>
2026-04-26 02:02:23 +0200

    add tabbed pane layout for main frame

--- src/com/alex/financetracker/ui/MainFrame.java
+++ src/com/alex/financetracker/ui/MainFrame.java
@@ -1,18 +1,28 @@
 package com.alex.financetracker.ui;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
 
     public MainFrame() {
+
         setTitle("Finance Tracker");
         setSize(900, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        JLabel label = new JLabel("Finance Tracker", SwingConstants.CENTER);
-        add(label);
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel incomePanel = new JPanel();
+        JPanel expensePanel = new JPanel();
+        JPanel reportPanel = new JPanel();
+
+        tabbedPane.addTab("Incomes", incomePanel);
+        tabbedPane.addTab("Expenses", expensePanel);
+        tabbedPane.addTab("Reports", reportPanel);
+
+        add(tabbedPane);
     }
 }
